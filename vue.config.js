d030962b91218e708bcaecb746a57b8625383a05
Faytelson <dfaitelson@scid.ru>
2024-01-10 16:11:17 +0300

    deploy-3

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/cartoon-fight/dist/" : "/",
   css: {
     loaderOptions: {
       scss: {
